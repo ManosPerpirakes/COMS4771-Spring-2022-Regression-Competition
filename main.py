e7139589a1dbf95e916b0ab565cb39303e95e8f6
Εμμανουήλ Περπιράκης <139139695+ManosPerpirakes@users.noreply.github.com>
2024-04-29 13:05:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsRegressor
+
+def get_month(var):
+    return int(var[0]+var[1])
+
+def get_day(var):
+    return int(var[3]+var[4])
+
+def set_data(dataframe):
+    dataframe["feature_11"] = dataframe["feature_0"].apply(get_month)
+    dataframe["feature_0"] = dataframe["feature_0"].apply(get_day)
+    return dataframe
+
+df = pd.read_csv("train_examples.csv")
+df = set_data(df)
+df_test = pd.read_csv("test_examples.csv")
+df_test = set_data(df_test)
+X = df
+y = pd.read_csv("train_labels.csv")["duration"]
+sc = StandardScaler()
+X_train = sc.fit_transform(X)
+regressor = KNeighborsRegressor(n_neighbors=300)
+regressor.fit(X, y)
+y_pred = regressor.predict(df_test)
+lst = []
+for i in range(100000):
+    lst.append(i)
+result = pd.DataFrame({"id": lst})
+result["duration"] = y_pred
+result.to_csv("results.csv", index=False)
+print("DONE")
